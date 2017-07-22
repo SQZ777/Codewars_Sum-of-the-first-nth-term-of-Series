12b41068b37bd0004bb7a7c222b9d471b452cee5
sqz777 <sqz777isme@gmail.com>
2017-07-22 13:48:45 +0800

    test input 2 expected 1.25

--- NthSeries/NthSeries/NthSeriesTest.cs
+++ NthSeries/NthSeries/NthSeriesTest.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable PossibleLossOfFraction
+// ReSharper disable SpecifyACultureInStringConversionExplicitly
 
 namespace NthSeries
 {
@@ -18,11 +20,20 @@ namespace NthSeries
             seriesResult("1", 1);
         }
 
+        [TestMethod]
+        public void Input_2_Should_Be_1_25()
+        {
+            seriesResult("1.25",2);
+        }
+
+        
+
         private static void seriesResult(string expected, int input)
         {
             var nthSeries = new NthSeries();
             Assert.AreEqual(expected, nthSeries.seriesSum(input));
         }
+
     }
 
     public class NthSeries
@@ -32,7 +43,13 @@ namespace NthSeries
             if (input.Equals(0))
                 return "0.00";
 
-            return input.ToString();
+            float result = 1.00f;
+            for (int i = 4; i < input * 3; i += 3)
+            {
+                result += 1 / float.Parse(i.ToString());
+            }
+            return result.ToString();
         }
+
     }
 }
