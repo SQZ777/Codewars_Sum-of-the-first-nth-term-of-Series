40f37c3a7f5bccc254eb7747a326aff976627bfb
sqz777 <sqz777isme@gmail.com>
2017-07-22 12:42:48 +0800

    Add the test project

---
+++ Sum_the_first_nth_term_Series/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sum_the_first_nth_term_Series
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
